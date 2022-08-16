37b75b985ad7015d893f5affbf6205a097134794
Mustafa Zakria <mustafa.zakria21@gmail.com>
2022-08-16 15:27:49 +0200

    updated Product class

--- app/src/main/java/com/example/clothingapp/ui/dataclasses/Product.kt
+++ app/src/main/java/com/example/clothingapp/ui/dataclasses/Product.kt
@@ -2,7 +2,6 @@ package com.example.clothingapp.ui.dataclasses
 
 data class Product(var price: Int,  var name: String, var image: String
                    ,var size:String="", var description: String="", var color: String=""
-                   , var id: Int=0, var isAddedToCart: Boolean=true,
-                   var boughtItemsCount: Int=0, var category: String="")
+                   , var id: Int=0, var isAddedToCart: Boolean=true)
 
 
